8cf21f7f0ad86367e31b23734b1cc86a4dadfb0f
ahw <andrewhallagan@gmail.com>
2015-09-27 11:47:44 -0700

    Update commends in example script

--- examples/test.js.bufwritepost.vimhook.buffer-output.sh
+++ examples/test.js.bufwritepost.vimhook.buffer-output.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# This VimHook just runs Node with whatever file triggered the BufWritePost
-# event. The hook will fire for any file ending with "test.js", which
-# probably only be test.js itself.
+# The hook will fire for any file ending with "test.js", which
+# probably only be test.js itself. It will simple run node with that
+# filename as an argument. Stdout is dumped to a scratch buffer that
+# automatically opens in a split window. If the scratch buffer already
+# exists it will be updated.
 
+# Dump stdout to scratch buffer
 # vimhook.bufferoutput
+
+# Whether or not to open in a vertical split (default horizontal)
+# vimhook.bufferoutput.vsplit = false
 
 node $1
